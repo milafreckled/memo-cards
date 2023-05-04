eeb85e61827387168364057da5ff0ff4b616e6e6
milafreckled <miladul2014@gmail.com>
2023-05-04 12:30:11 +0200

    Revert "first commit"
    This reverts commit 61a9220c4be451b93440ea1205b48dfb30452d83.

--- memo-cards/src/app/components/card/reusableAnimation.ts
+++
@@ -1,9 +0,0 @@
-import { animation , style, animate} from "@angular/animations";
-
-export const reusableAnimation = animation([
-    style({
-        opacity: '{{ opacity }}' || 0,
-        backgroundImage: '{{ backgroundImage }}' || 'none'
-    }),
-    animate('{{ time }} {{ animateFucntion }}')
-])
